c2a92816b0f7758b90ef3172aeb035c6ef62ec08
steph <stephguiragossian@gmail.com>
2025-08-19 20:32:17 +0300

    bug fix

--- backend/controllers/clerkWebhooks.js
+++ backend/controllers/clerkWebhooks.js
@@ -2,43 +2,44 @@ import User from "../models/User.js";
 import { Webhook } from "svix";
 
 const clerkWebhooks = async (req, res) => {
-    try {
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        const headers = {
-            "svix-id": req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"],
-        }
-        await whook.verify(JSON.stringify(req.body), headers)
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+    const headers = {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    };
 
-        const { data, type } = req.body
-        const userData = {
-            _id: data.id,
-            email: data.email_addresses[0].email_address,
-            username: data.first_name + " " + data.last_name,
-            image: data.image_url
-        }
-        switch (type) {
-            case "user.created": {
-                await User.create(userData)
-                break;
-            }
-            case "user.updated": {
-                await User.findByIdAndUpdate(data.id)
-                break;
-            }
-            case "user.deleted": {
-                await User.findByIdAndDelete(data.id)
-                break;
-            }
-            default:
-                break;
-        }
-        res.json({ success: true, message: "Webhook Received" })
-    } catch (error) {
-        console.log(error.message);
-        res.status(400).json({ success: false, message: error.message })
+    const payloadString = req.body.toString();
+    const evt = whook.verify(payloadString, headers);
+    const { data, type } = JSON.parse(payloadString);
+
+    const userData = {
+      _id: data.id,
+      email: data.email_addresses[0].email_address,
+      username: data.first_name + " " + data.last_name,
+      image: data.image_url,
+      recentSearchedCities: [], // prevent validation error
+    };
+
+    switch (type) {
+      case "user.created":
+        await User.create(userData);
+        break;
+      case "user.updated":
+        await User.findByIdAndUpdate(data.id, userData, { new: true });
+        break;
+      case "user.deleted":
+        await User.findByIdAndDelete(data.id);
+        break;
     }
-}
+
+    res.json({ success: true, message: "Webhook Received" });
+  } catch (error) {
+    console.log("Webhook Error:", error.message);
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
 
 export default clerkWebhooks
